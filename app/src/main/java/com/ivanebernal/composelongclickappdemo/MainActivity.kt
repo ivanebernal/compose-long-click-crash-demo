2858076a2f7b6b60981bf1a0bb0ddeadd9c5118b
Iván Esparza <ivane@squareup.com>
2023-04-14 15:27:33 -0700

    Implement debouncing clicks

--- app/src/main/java/com/ivanebernal/composelongclickappdemo/MainActivity.kt
+++ app/src/main/java/com/ivanebernal/composelongclickappdemo/MainActivity.kt
@@ -3,9 +3,9 @@ package com.ivanebernal.composelongclickappdemo
 import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_CANCEL
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -15,26 +15,52 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // val activity2Intent = Intent(this, Activity2::class.java)
-    setContent {
-      CrashButton {
-        dropLateEvents()
-        // startActivity(activity2Intent)
+    val activity2Intent = Intent(this, Activity2::class.java)
+    val startActivityWithDelay = {
+      allowEvents = false
+      runBlocking {
+        delay(200)
+        startActivity(activity2Intent)
       }
     }
+    setContent {
+      CrashButton(
+        onClick = startActivityWithDelay,
+        onLongClick = startActivityWithDelay
+      )
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    allowEvents = true
+  }
+
+  private var allowEvents = true
+  set(value) {
+    val was = field
+    field = value
+    if (was != value) {
+      dropLateEvents()
+    }
+  }
+
+  override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+    return !allowEvents || super.dispatchTouchEvent(ev)
+  }
+
+  override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+    return !allowEvents || super.dispatchKeyEvent(event)
   }
 
   private fun dropLateEvents() {
@@ -48,18 +74,16 @@ class MainActivity : AppCompatActivity() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CrashButton(onLongClick: () -> Unit) {
-  val context = LocalContext.current
+fun CrashButton(
+  onClick: () -> Unit = {},
+  onLongClick: () -> Unit = {},
+) {
   Box(
     modifier = Modifier
       .size(120.dp, 40.dp)
       .background(color = Color.Gray)
       .combinedClickable(
-        onClick = {
-          Toast
-            .makeText(context, "Click", Toast.LENGTH_SHORT)
-            .show()
-        },
+        onClick = onClick,
         onLongClick = onLongClick
       )
   ) {
@@ -70,7 +94,5 @@ fun CrashButton(onLongClick: () -> Unit) {
 @Preview
 @Composable
 fun Preview() {
-  CrashButton {
-    // nothing
-  }
+  CrashButton()
 }
